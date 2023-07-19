56dea55cbc746d59bd26227ba7a623385c9db8ba
fancn21th <fancn21th@gmail.com>
2023-07-19 15:41:32 +0800

    simple llmchain

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import "dotenv/config";
-
 import { OpenAI } from "langchain/llms/openai";
-
-console.log(process.env.OPENAI_API_KEY);
+import { PromptTemplate } from "langchain/prompts";
+import { LLMChain } from "langchain/chains";
 
 const model = new OpenAI({
-  openAIApiKey: process.env.OPENAI_API_KEY,
   temperature: 0.9,
+  openAIApiKey: process.env.OPENAI_API_KEY,
+});
+const template = "What is a good name for a company that makes {product}?";
+const prompt = new PromptTemplate({
+  template: template,
+  inputVariables: ["product"],
 });
 
-const res = await model.call(
-  "What would be a good company name a company that makes colorful socks?"
-);
-
+const chain = new LLMChain({ llm: model, prompt: prompt });
+const res = await chain.call({ product: "colorful socks" });
 console.log(res);
